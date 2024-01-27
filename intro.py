dc3dd896afce009f3478ce1a180abaf6b5443e12
Varun Singhal <125869926+varunsinghal7@users.noreply.github.com>
2024-01-27 23:46:24 +0530

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,2 +1,2 @@
 print("Hello!, my name is Varun Singhal")
-print("I am currently learning Python and Web development")
+print("I am currently learning Python and Web development. I am also interested in Data Science and Data analytics")
